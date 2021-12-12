c2bee5eb767a15e3238da3138aa4b224c9c18cfa
Your Name <you@example.com>
2021-12-12 17:31:28 -0500

    update

---
+++ Note07_For_While.java
@@ -0,0 +1,19 @@
+/*
+
+for (条件初始；条件判断；条件控制) {
+    循环内代码
+}
+
+*/
+
+public class Note07_For_While {
+    public static void main(String[] args) {
+
+        for(int i = 1; i<=5; i++) {
+            System.out.println(i + " is less than 6");
+        }
+
+
+
+    }
+}
