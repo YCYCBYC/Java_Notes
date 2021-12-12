a205eb6abb377c5d23cf203dd1c06d3105ff676d
Your Name <you@example.com>
2021-12-12 14:31:40 -0500

    Update 12.12

---
+++ Note02_Constant.java
@@ -0,0 +1,17 @@
+// 常量
+
+class Note02_Constant {
+    public static void main(String[] args) {
+        System.out.println("Cheng");
+        System.out.println("Support Chinese?");
+        
+        System.out.println(123);
+        System.out.println(-12);
+        
+        System.out.println(12.34);
+        System.out.println(-1.23);
+
+        System.out.println(true);
+        System.out.println(false);
+    }
+}
---
+++ Note03_Variable.java
@@ -0,0 +1,14 @@
+//  byte short int* long
+//  float double*
+//  char
+//  boolean
+
+class Note03_Variable {
+    public static void main(String[] args) {
+        short a = 10;
+        System.out.println(a);
+
+        a = 20;
+        System.out.println(a);
+    }
+}
