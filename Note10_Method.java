fe94ff1877afd670af9ff762c5710fc15a80537c
YCYCBYC <ycycbyc@outlook.com>
2021-12-21 10:59:21 -0500

    update 10

--- Note10_Method.java
+++ Note10_Method.java
@@ -1,7 +1,25 @@
 public class Note10_Method {
     public static void main(String[] args) {
+        
+        isEven();
+
+    }
 
 
+    //  Define a menthod
+    public static void isEven() {  
+        int number = 9;
+        if(number%2 == 0) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
+
+    
+    // Define method with parameters
+    public static void isOdd() {
         
     }
+
 }
