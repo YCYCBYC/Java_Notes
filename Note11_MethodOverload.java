8973136bc8942781dc37a3d30f7c16670286d79a
YCYCBYC <ycycbyc@outlook.com>
2021-12-23 15:46:57 -0500

    update method

---
+++ Note11_MethodOverload.java
@@ -0,0 +1,33 @@
+//  方法重载：
+//  多个方法在一个class中
+//  多个方法具有同样的方法名
+//  多个方法的参数不同：类型不同或者数量不同
+
+
+public class Note11_MethodOverload {
+    public static void main(String[] args) {
+
+        System.out.println(sum(1,2));
+        System.out.println(sum(1.1,2.1));
+        System.out.println(sum(1,2,3));
+
+    }
+
+
+    //  Method 1
+    public static int sum(int a, int b) {
+        return a+b;
+    }
+
+
+    //  Method 2
+    public static double sum(double a , double b) {
+        return a+b;
+    }
+
+
+    // Method 3
+    public static int sum(int a, int b, int c) {
+        return a+b+c;
+    }
+}
