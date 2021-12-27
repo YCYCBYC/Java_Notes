4ab8ad34f6f7a7a86a8854d86efd2676870ea9f2
YCYCBYC <ycycbyc@outlook.com>
2021-12-27 18:16:12 -0500

    private

---
+++ Note15_Private.java
@@ -0,0 +1,42 @@
+import javax.swing.plaf.basic.BasicTreeUI.NodeDimensionsHandler;
+
+//  private 变量只能在这个class中使用
+//  定义方法 set, get 来从别的地方调用值
+
+
+public class Note15_Private {
+    
+    //  define variables
+    private int age;
+    private String name; 
+
+    //  define method of set variables
+    public void setAge(int a) {
+        if(a<0 || a>120) {
+            System.out.println("Entered name is not real.");
+        } else {
+            age = a;
+            System.out.println("Age has been assigned.");
+        }
+    }
+    public void setName(String n) {
+        name = n;
+    }
+
+
+    //  define method of set variables
+    public int getAge() {
+        return age;
+    }
+    public String getName() {
+        return name;
+    }
+
+
+    //define method
+    public void show() {
+        System.out.println(name + " " + age);
+    }
+
+
+}
