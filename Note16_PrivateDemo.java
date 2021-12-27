cd0398be671cd0be7c0f99f9944b4b72d8cd6fd0
YCYCBYC <ycycbyc@outlook.com>
2021-12-27 18:16:19 -0500

    provate

---
+++ Note16_PrivateDemo.java
@@ -0,0 +1,26 @@
+public class Note16_PrivateDemo {
+    public static void main(String[] args) {
+
+        //  create new object
+        Note15_Private stu = new Note15_Private();
+        
+        
+        //  assign values
+        stu.setName("Cheng");
+        stu.setAge(-4);
+        stu.setAge(22);
+        
+
+        //  use method
+        stu.show();
+        System.out.println("--------");
+
+
+        //  get values
+        System.out.println(stu.getName());
+        System.out.println(stu.getAge());
+
+
+
+    }
+}
