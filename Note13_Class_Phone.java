22775e1cb2ac92f5c3ba63a38e2ecfce0a32ead5
YCYCBYC <ycycbyc@outlook.com>
2021-12-27 17:51:34 -0500

    object and class

---
+++ Note13_Class_Phone.java
@@ -0,0 +1,23 @@
+//  Note13_Class_Phone  是类名，无法运行
+//  brand, price 是成员变量 / 方法内的变量testvar,是局部变量
+//  Note14_Object_Phone  是测试类，需要main方法
+
+
+public class Note13_Class_Phone {
+    
+    //  variables
+    String brand;
+    int price;
+
+    //  methods
+    public void call() {
+        System.out.println("Make a ohone call");
+    }
+
+    public void message() {
+        System.out.println("Send a text message");
+        //  int testvar = 0;
+    }
+
+
+}
---
+++ Note14_Object_Phone.java
@@ -0,0 +1,40 @@
+//  Note13_Class_Phone  是类名，无法运行
+//  Note14_Object_Phone  是测试类，需要main方法
+
+
+public class Note14_Object_Phone {
+    public static void main(String[] args) {
+
+        //  create an object
+        Note13_Class_Phone p = new Note13_Class_Phone();
+        //  use variables without value
+        System.out.println(p.brand);
+        System.out.println(p.price);
+        System.out.println("--------");
+        //  assign value to variables
+        p.brand = "SAMSUNG";
+        p.price = 899;
+        System.out.println(p.brand);
+        System.out.println(p.price);
+        System.out.println("--------");
+        // use method
+        p.call();
+        p.message();
+        System.out.println("--------");
+
+
+        //  create another object with same class
+        Note13_Class_Phone ps = new Note13_Class_Phone();
+        ps.brand = "Apple";
+        ps.price = 1099;
+        //  use variables
+        System.out.println(ps.brand);
+        System.out.println(ps.price);
+        System.out.println("--------");
+        // use method
+        ps.call();
+        ps.message();
+        System.out.println("--------");
+
+    }
+}
